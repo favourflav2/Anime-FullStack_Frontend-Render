8a961fb707b50d305516f7478947f197e7b199f9
favourflav <chukwagon1@gmail.com>
2023-03-12 11:42:26 -0700

    edit

--- src/pages/MyFavoriteAnime.jsx
+++ src/pages/MyFavoriteAnime.jsx
@@ -67,6 +67,21 @@ export default function MyFavoriteAnime() {
     setTopTierState(result);
   }, [state?.bestAnime, arrayValue]);
 
+  if (state.loading) {
+    return (
+      <div className="flex items-center justify-center space-x-2">
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-danger motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col bg-[#161623]">
       <div className="flex justify-center my-10">
